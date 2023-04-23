9a384c3a1ab16809459a32b14ce0fc111f06ffae
Lip-7 <zhighofficial@gmail.com>
2023-04-23 16:06:48 +0200

    added case if there aren't results

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,18 +1,25 @@
 <template>
     <main>
-        <MoviesList/>
-        <SeriesList/>
+        <MoviesList v-if="store.movieList.length > 0" />
+        <div v-else class="alert alert-danger mt-2">
+            <h3>Non ci sono Film che rispettano le tue ricerche</h3>
+        </div>
+        <SeriesList v-if="store.seriesList.length > 0" />
+        <div v-else class="alert alert-danger mt-2">
+            <h3>Non ci sono Serie che rispettano le tue ricerche</h3>
+        </div>
     </main>
 </template>
   
 <script>
+import { store } from '../store/store'
 import SeriesList from './SeriesList.vue';
 import MoviesList from './MoviesList.vue';
 export default {
     name: 'MainComponent',
     data() {
         return {
-
+            store,
         }
     },
     methods: {
@@ -29,7 +36,7 @@ export default {
 </script>
   
 <style lang="scss" scoped>
-main{
+main {
     padding-top: 70px;
 }
 </style>
